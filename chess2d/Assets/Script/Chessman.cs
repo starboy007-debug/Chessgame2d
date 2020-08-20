ba31cf7f0b0cc81db6164bd3a02dc6224219d786
starboy007-debug <65112799+starboy007-debug@users.noreply.github.com>
2020-08-20 23:33:30 +0530

    updatecode

---
+++ chess2d/Assets/Script/Chessman.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Chessman : MonoBehaviour
+{
+
+    public GameObject controller;
+    public GameObject moveplate;
+
+    private int xBoard = -1;
+    private int yBoard = -1;
+
+    private string player;
+
+    public Sprite black_queen, black_knight, black_bishop, black_king, black_rook, black_pawn;
+    public Sprite white_queen, white_knight, white_bishop, white_king, white_rook, white_pawn;
+
+
+
+    public void Activate()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+
+        SetCords();
+
+        switch (this.name)
+        {
+            case "black_queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
+            case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
+            case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
+            case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
+            case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
+            case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
+            case "white_queen": this.GetComponent<SpriteRenderer>().sprite = white_queen; break;
+            case "white_knight": this.GetComponent<SpriteRenderer>().sprite = white_knight; break;
+            case "white_bishop": this.GetComponent<SpriteRenderer>().sprite = white_bishop; break;
+            case "white_king": this.GetComponent<SpriteRenderer>().sprite = white_king; break;
+            case "white_rook": this.GetComponent<SpriteRenderer>().sprite = white_rook; break;
+            case "white_pawn": this.GetComponent<SpriteRenderer>().sprite = white_pawn; break;
+        }
+
+
+    }
+
+    public void SetCords()
+    {
+        float x = xBoard;
+        float y = yBoard;
+
+        x *= 0.66f;
+        y *= 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+
+        this.transform.position = new Vector3(x, y, -1.0f);
+    }
+
+
+
+}
