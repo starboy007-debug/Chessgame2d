bc08828d09e7cd26f75828b2a96734f3ada1e779
starboy007-debug <65112799+starboy007-debug@users.noreply.github.com>
2020-08-20 22:40:34 +0530

    update

---
+++ chess2d/Assets/Script/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject Chesspiece;
+    void Start()
+    {
+        Instantiate(Chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
